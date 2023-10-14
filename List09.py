6c8563e58df00773192d3adadb368bb168cbdec3
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-14 07:01:40 +0500

    kod yozdim

--- List09.py
+++ List09.py
@@ -6,4 +6,14 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    i=0
+    s=[]
+    j=0
+    while i<len(fruits):
+        if fruits[i]=="apple":
+            s.append(i)
+            j+=1
+        i+=1
+    s.insert(0,j)
+    return s
+print(main(["apple", "banana", "apple", "pear", "apple"]))
